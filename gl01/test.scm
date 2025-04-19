0d32412802d72b4e3647a533b0c9f7b2ffc98492
sunda <8mo10dance@protonmail.com>
2025-04-19 21:18:54 +0900

    Define rspec like macro

--- gl01/test.scm
+++ gl01/test.scm
@@ -1,10 +1,7 @@
-(use-modules (srfi srfi-64))
+(use-modules (rspec-like-dsl))
 (use-modules (deep-collection))
 
-(test-begin "deep-collection tests")
-
-(test-equal "deep double of '(1 2 (3 4)) should be '(2 4 (6 8))"
-  '(2 4 (6 8))
-  (deep-map (lambda (x) (* x 2)) '(1 2 (3 4))))
-
-(test-end "deep-collection tests")
+(describe "deep-collection tests"
+          (it "deep double of '(1 2 (3 4)) should be '(2 4 (6 8))"
+              (deep-map (lambda (x) (* x 2)) '(1 2 (3 4)))
+              '(2 4 (6 8))))
