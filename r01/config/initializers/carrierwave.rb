8fb0b2fe403449bc55694231be2bce2ad54a6677
sunda <136877788+8mo10dance@users.noreply.github.com>
2025-06-09 16:25:53 +0900

    test: use file storage for carrierwave

--- r01/config/initializers/carrierwave.rb
+++ r01/config/initializers/carrierwave.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.cache_storage = :fog
-  config.fog_directory = Settings.aws.s3.bucket
-  config.fog_credentials = {
-    provider: 'AWS',
-    region: Settings.aws.region,
-    aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    endpoint: Settings.aws.endpoint,
-    path_style: true
-  }
+  if Rails.env.test?
+    config.storage = :file
+    config.cache_storage = :file
+    config.root = Rails.root.join('tmp')
+  else
+    config.storage = :fog
+    config.cache_storage = :fog
+    config.fog_directory = Settings.aws.s3.bucket
+    config.fog_credentials = {
+      provider: 'AWS',
+      region: Settings.aws.region,
+      aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      endpoint: Settings.aws.endpoint,
+      path_style: true
+    }
+  end
 end
