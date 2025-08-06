e4fc484e2e287eff53809df786bb7959b9526051
sunda <sunda0707@icloud.com>
2025-08-07 01:07:58 +0900

    wip

---
+++ gl02/main.scm
@@ -0,0 +1,26 @@
+(define (pow base n)
+  (let* ((T (lambda (x) (* base x)))
+	 (Tn (accumulate T n)))
+    (Tn 1)))
+
+(define (fib n)
+  (let* ((T (lambda (v)
+	      (mtx-mul-by-vec '((1 1)
+				(1 0)) v)))
+	 (Tn (accumulate T n)))
+    (car (Tn '(1 0)))))
+
+(define (mtx-mul-by-vec A v)
+  (map (lambda (row)
+	 (apply + (map * row v)))
+       A))
+
+(define (accumulate T n)
+  (cond ((= n 0) (lambda (x) x))
+	(else
+	 (combine T
+		  (accumulate T (- n 1))))))
+
+(define (combine T1 T2)
+  (lambda (x)
+    (T2 (T1 x))))
