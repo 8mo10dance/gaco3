e2fdac0f7c557852711cf87ea7e9996629a88454
sunda <8mo10dance@protonmail.com>
2025-09-13 14:48:23 +0900

    Run rails new in dagger

--- r04/dagger.sh
+++ r04/dagger.sh
@@ -2,9 +2,17 @@
 
 set -eu
 
+RUBY_VERSION=${RUBY_VERSION:-}
+APP_NAME=${APP_NAME:-myapp}
+RAILS_NEW_OPTIONS=${RAILS_NEW_OPTIONS:-}
+
+echo "Ruby Version: ${RUBY_VERSION}"
+echo "To run: rails new ${APP_NAME} ${RAILS_NEW_OPTIONS}"
+
 dagger shell <<EOF
 container |
   build . --build-args RUBY_VERSION=${RUBY_VERSION} |
-  with-directory . . |
-  terminal
+  with-exec -- rails new /ruby/${APP_NAME} ${RAILS_NEW_OPTIONS} |
+  directory /ruby/${APP_NAME} |
+  export ./${APP_NAME}
 EOF
