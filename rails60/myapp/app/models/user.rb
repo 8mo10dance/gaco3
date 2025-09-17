272c3fc699b5191f04871a69bf3621bb0c0709ae
sunda <sunda@synchro-food.co.jp>
2025-09-17 19:14:28 +0900

    wip

--- rails60/myapp/app/models/user.rb
+++ rails60/myapp/app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   extend Enumerize
 
   enumerize :active, in: { inactive: 0, active: 1 }
+
+  def hoge
+    # TODO
+  end
+
+  def call
+    hoge(1)
+    hoge(1.1)
+    hoge('1.1')
+  end
 end
