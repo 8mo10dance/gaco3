0d7fb1505b1a7a19ae3eeb106704bf92b56c4148
sunda <sunda@synchro-food.co.jp>
2025-09-17 20:04:43 +0900

    wip5

--- rails60/myapp/lib/rubocop/cop/custom/no_hoge_with_numeric_arg_pattern.rb
+++ rails60/myapp/lib/rubocop/cop/custom/no_hoge_with_numeric_arg_pattern.rb
@@ -4,6 +4,8 @@ module RuboCop
   module Cop
     module Custom
       class NoHogeWithNumericArgPattern < Base
+        extend AutoCorrector
+
         MSG = 'Do not call `hoge` with a numeric argument.'.freeze
 
         def_node_matcher :hoge_with_value_hash?, <<~PATTERN
@@ -25,7 +27,29 @@ module RuboCop
           first_arg = node.arguments.first
           return unless first_arg.pairs.any? { |pair_node| value_int_pair?(pair_node) }
 
-          add_offense(node)
+          add_offense(node) do |corrector|
+            autocorrect_value_hash(corrector, node)
+          end
+        end
+
+        private
+
+        def autocorrect_value_hash(corrector, node)
+          first_arg = node.arguments.first
+          target_node = first_arg.pairs.find { |pair_node| value_int_pair?(pair_node) }
+          target_pair_arr = target_node.value.pairs.map do |pair|
+            [pair.key.value, int_to_boolean(pair.value.value)]
+          end
+          value_hash_str = target_pair_arr.map { |k, v| "#{k}: #{v}" }.join(', ')
+
+          new_code = <<~RUBY.chomp
+            value: { #{value_hash_str} }
+          RUBY
+          corrector.replace(target_node, new_code)
+        end
+
+        def int_to_boolean(val)
+          val == 1
         end
       end
     end
