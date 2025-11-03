b59cbafc213ff83f79964f5c255b75af2c68b393
sunda <8mo10dance@protonmail.com>
2025-11-03 14:55:46 +0900

    Remove default from text column

--- r01/db/migrate/20231123083046_create_posts.rb
+++ r01/db/migrate/20231123083046_create_posts.rb
@@ -3,7 +3,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.text :content, null: false, default: ''
+      t.text :content, null: false
       t.string :image, null: false, default: ''
       t.references :user, null: false, foreign_key: true
 
